2563192f4c9f5d40c7603e7fdfb8a61b9ee10e4a
fukai1217678293 <1217678293@qq.com>
2017-08-14 20:04:00 +0800

    clear project

--- Pods/Target Support Files/Pods-VouteStatement/Pods-VouteStatement-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VouteStatement : NSObject
-@end
-@implementation PodsDummy_Pods_VouteStatement
-@end
--- Pods/Target Support Files/Pods-VouteStatementTests/Pods-VouteStatementTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VouteStatementTests : NSObject
-@end
-@implementation PodsDummy_Pods_VouteStatementTests
-@end
